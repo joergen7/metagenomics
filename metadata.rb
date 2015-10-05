39e24b2bb9150404312f212bbb19f2327fcbf296
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-10-05 11:41:15 +0200

    Metadata updated.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,14 @@
 name             'metagenomics'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
+maintainer       "Jörgen Brandt"
+maintainer_email "brandjoe@hu-berlin.de"
 license          'all_rights'
 description      'Installs/Configures metagenomics'
 long_description 'Installs/Configures metagenomics'
 version          '0.1.0'
 
+recipe "metagenomics::_common", "Performs an update of the package system."
+recipe "metagenomics::default", "Sets up all preconditions to run the metagenomics workflow."
+
+
+depends "chef-bioinf-worker"
+depends "chef-cuneiform"
