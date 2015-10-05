1c57b66995a3c8ff97194962474da3880b39e16d
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-10-05 10:12:42 +0200

    Dependent cookbooks pulled into default recipe.

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,11 @@
 #
 # Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
 
+
+include_recipe "chef-bioinf-worker::qiime"
+include_recipe "chef-cuneiform::default"
+
+
 mphm_dir = "#{node.dir.data}/mphm"
 
 
