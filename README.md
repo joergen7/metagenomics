22611b9dbb289c121eb73850f7cc93c0da444eb4
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-10-12 10:34:43 +0200

    Green genomes OTUs added.

--- README.md
+++ README.md
@@ -38,7 +38,7 @@ You can drop the VM by entering
 
 The Cuneiform workflow can be found in the cookbook under
 
-    cookbooks/mirna/templates/default/metagenomics.cf.erb
+    cookbooks/metagenomics/templates/default/metagenomics.cf.erb
     
 and on the test machine under
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -14,6 +14,7 @@ mphm_dir = "#{node.dir.data}/mphm"
 
 directory node.dir.wf
 directory node.dir.data
+directory node.dir.archive
 
 # place workflow
 template "#{node.dir.wf}/metagenomics.cf" do
@@ -36,3 +37,20 @@ node.data.mphmrange.each { |id|
   end
   
 }
+
+# download greengenes otu reference
+
+otu_link = "ftp://greengenes.microbio.me/greengenes_release/gg_13_5/gg_13_8_otus.tar.gz"
+otu_tar  = "#{node.dir.archive}/#{File.basename( otu_link )}"
+otu_dir  = "#{node.dir.data}/gg_13_8_otus"
+
+remote_file otu_tar do
+  action :create_if_missing
+  source otu_link
+  retries 1
+end
+
+bash "extract_otu" do
+  code "tar xf #{otu_tar} -C #{node.dir.data}"
+  not_if "#{Dir.exists?( otu_dir )}"
+end
